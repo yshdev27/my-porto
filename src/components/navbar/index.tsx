14ba44c9e09c0719f5e053ef8e4687c3261dee44
Yash Gupta <gyash21@gmail.com>
2025-06-02 17:29:46 +0530

    update- pfp done

--- src/components/navbar/index.tsx
+++ src/components/navbar/index.tsx
@@ -1,10 +1,12 @@
+"use client";
 import React from 'react'
 import Image from 'next/image'
+import Link from 'next/link'
 import { Container } from '../container'
 
 export const Navbar = () => {
 
-    const navItemss = [
+    const navItems = [
         {title: 'About',
         href: '/about'},
 
@@ -21,9 +23,17 @@ export const Navbar = () => {
 
   return (
     <Container>
-        <div className='flex items-center justify-between'>
-        <Image src="pro-pic1.jpg" height="100" width="100" alt="Yash Picture"/>
+        <nav className='flex items-center justify-between p-2'>
+        <Image className='h-14 w-14 rounded-full' src="/pro-pic1.jpg" height="100" width="100" alt="Yash Picture"/>
+
+        <div className='flex items-center'>
+            {navItems.map((item, idx) => (
+                <Link className='text-sm' href={item.href} key={idx}>
+                    {item.title}
+                </Link>
+            ))}
         </div>
+        </nav>
     </Container>
   )
 }
